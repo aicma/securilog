05574c84044fa94ef2b37e668cd7ebe9c6120dbd
aicma <Alec7@web.de>
2016-05-04 11:12:52 +0200

    server debug

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var PORT = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var mongoData = 'mongodb://alex:securilog@s013340.mlab.com:13340/heroku_2zhtbsdn';
 
@@ -9,4 +10,6 @@ app.get('/', function(req, res){
     res.send('get request received');
 });
 
-app.post
+app.listen(PORT, function(){
+  console.log('listening on Port: '+ PORT);
+})
