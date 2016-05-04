fb8d79e1a44da9b0153480195a84c731d3e2bb62
aicma <Alec7@web.de>
2016-05-04 12:05:34 +0200

    cleaning up the mess

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
-var mongoose = require('mongoose');
-var mongoData = 'mongodb://heroku_2:heroku_2@s013340.mlab.com:13340/heroku_2zhtbsdn';
 
-mongoose.connect(mongoData);
+var mongoose = require('mongoose');
+var mongoDbUri = 'mongodb://heroku_2:heroku_2@ds013340.mlab.com:13340/heroku_2zhtbsdn';
+mongoose.connect(mongoDbUri);
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
 
 app.get('/', function(req, res){
     res.send('get request received');
