44afcd1b3201a0c29d3672eaf10b6afc8f2efe83
aicma <Alec7@web.de>
2016-05-04 11:27:42 +0200

    mongodb test

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
 var mongoose = require('mongoose');
-var mongoData = 'mongodb://alex:securilog@s013340.mlab.com:13340/heroku_2zhtbsdn';
+var mongoData = 'mongodb://heroku_2:heroku_2@s013340.mlab.com:13340/heroku_2zhtbsdn';
 
 mongoose.connect(mongoData);
 
